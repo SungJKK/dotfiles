0da7f3762ac76ef9597b5944e391a6c424b8b450
Sung J Kim <1998sungjoong@gmail.com>
2021-05-15 19:35:25 -0700

    Second test for script

--- __scripts__/bootstrap.sh
+++ __scripts__/bootstrap.sh
@@ -16,9 +16,9 @@ init_proc() {
 
   if [[ $CONTINUE = 'y' ]];then
     download_pkgs
-    remove_prev_files
-    create_links
-    change_shell
+    clean_state
+    # create_links
+    # change_shell
   else
     echo -e "Aborting..."
     exit
@@ -28,42 +28,69 @@ init_proc() {
 # Download packages
 download_pkgs() {
   echo -e "Downloading packages..."
-  make -f $DOT_DIR/__scripts__/InstallPkgs.mk
+  make -f $DOT_DIR/__scripts__/ArchPkgs
 }
 
 # Remove any previous files
-remove_prev_files() {
-  echo -e "Removing previous files..."
+clean_state() {
+  echo -e "Cleaning previous files..."
+  sudo rm -rf $CONF_DIR/kitty/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/kitty
+
   sudo rm -rf ~/.gitconfig ~/.gitignore_global > /dev/null 2>&1
-  sudo rm -rf ~/.config/kitty/* > /dev/null 2>&1
-  sudo rm -rf ~/.config/htop/* > /dev/null 2>&1
-  sudo rm -rf ~/.config/bashtop/* > /dev/null 2>&1
-  sudo rm -rf ~/.config/neofetch/* > /dev/null 2>&1
-  sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
-  sudo rm -rf ~/.zshenv ~/.config/zsh/* > /dev/null 2>&1
+
+  sudo rm -rf $CONF_dir/htop/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/htop
+
+  sudo rm -rf $CONF_DIR/bashtop/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/bashtop
+
+  sudo rm -rf $CONF_DIR/neofetch/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/neofetch
+
+  sudo rm -rf $CONF_DIR/tmux/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/tmux
+
+  sudo rm -rf ~/.zshenv $CONF_DIR/zsh/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/zsh
+
   sudo rm -rf ~/.vim/* > /dev/null 2>&1
-  sudo rm -rf ~/.zathura/* > /dev/null 2>&1
-  sudo rm -rf ~/Programs/weechat/* > /dev/null 2>&1
-  sudo rm -rf ~/.config/neomutt/* > /dev/null 2>&1
-  sudo rm -rf ~/.newsboat/* > /dev/null 2>&1
-  sudo rm -rf ~/.config/nvim/* > /dev/null 2>&1
-  sudo rm -rf ~/.config/ranger/* > /dev/null 2>&1
+  mkdir -p ~/.vim
+
+  sudo rm -rf $CONF_DIR/zathura/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/zathura
+
+  sudo rm -rf $CONF_DIR/weechat/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/weechat
+
+  sudo rm -rf $CONF_DIR/neomutt/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/neomutt
+
+  sudo rm -rf $CONF_DIR/newsboat/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/newsboat
+
+  sudo rm -rf $CONF_DIR/nvim/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/nvim
+
+  sudo rm -rf $CONF_DIR/ranger/* > /dev/null 2>&1
+  mkdir -p $CONF_DIR/ranger
 }
 
 # Create symlinks
 create_links() {
   echo -e "Creating symlinks..."
-  ln -sf $DOT_DIR/kitty/* $CONF_DIR/kitty/
+  ln -sf $DOT_DIR/kitty $CONF_DIR/kitty
 
   ln -sf $DOT_DIR/gitconfig/gitconfig ~/.gitconfig
   ln -sf $DOT_DIR/gitconfig/gitignore ~/.gitignore_global
 
   ln -sf $DOT_DIR/tops/htoprc $CONF_DIR/htop/htoprc
+
   ln -sf $DOT_DIR/tops/bashtop.cfg $CONF_DIR/bashtop/bashtop.cfg
 
   ln -sf $DOT_DIR/neofetch $CONF_DIR/neofetch
 
-  ln -sf $DOT_DIR/tmux/tmux.conf ~/.tmux.conf
+  ln -sf $DOT_DIR/tmux $CONF_DIR/tmux
 
   ln -sf $DOT_DIR/zsh/zshenv ~/.zshenv
   ln -sf $DOT_DIR/zsh/zshrc $CONF_DIR/zsh/.zshrc
@@ -73,6 +100,16 @@ create_links() {
   ln -sf $DOT_DIR/vim ~/.vim
 
   ln -sf $DOT_DIR/zathura $CONF_DIR/zathura
+
+  ln -sf $DOT_DIR/weechat/irc.conf ~/Programs/weechat/irc.conf
+
+  ln -sf $DOT_DIR/neomutt $CONF_DIR/neomutt
+
+  ln -sf $DOT_DIR/newsboat $CONF_DIR/newsboat
+
+  ln -sf $DOT_DIR/nvim $CONF_DIR/nvim
+
+  ln -sf $DOT_DIR/ranger $CONF_DIR/ranger
 }
 
 # Set default shell
@@ -84,47 +121,6 @@ change_shell () {
 # Start program
 init_proc
 
-
-# ==> gitconfig
-  # path : ~/.gitconfig & ~/.gitignore_global
-
-# ==> kitty
-  # path : ~/.config/kitty/*
-
-# ==> tops
-  # path : ~/.config/htop/htoprc
-  # path : ~/.config/bashtop/bashtop.cfg
-
-# ==> neofetch
-  # path : ~/.config/neofetch/config.conf
-
-# ==> tmux 
-  # path : ~/.tmux.conf
-
-# ==> zsh   
-  # path : ~/.zshenv
-  #      : ~/.config/zsh/{.zshrc,.zprofile,.p10k.zsh}
-
-# ==> vim 
-  # path : ~/.vim/*
-
-# ==> zathura
-  # path : ~/.config/zathura/zathurarc
-
-# ==> weechat
-  # path : ~/Programs/weechat/
-
-# ==> neomutt
-  # path : ~/.config/neomutt/
-
-# ==> newsboat
-  # path : ~/.newsboat/
-
-# ==> neovim
-  # path : ~/.config/nvim/*
-
-# ==> ranger
-  # path : ~/.config/ranger/rc.conf
 
 
 
